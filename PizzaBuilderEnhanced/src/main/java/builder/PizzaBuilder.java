14a87c6706422a2f81ec94f1b8bb1d0f5e885067
RawanMohamedSalah <mseldeen1@gmail.com>
2024-03-13 20:57:10 +0200

    Intial Commit

---
+++ PizzaBuilderEnhanced/src/main/java/builder/PizzaBuilder.java
@@ -0,0 +1,9 @@
+package builder;
+
+public interface PizzaBuilder {
+		PizzaBuilder setSize(String size);
+	    PizzaBuilder setCrust(String crust);
+	    PizzaBuilder addTopping(String topping);
+	    Pizza build();
+	}
+
